ad877f1d50925de272a0605e74ec6cfa2a758526
dcienkuszewski <dominik.cienkuszewski@strix.net>
2025-12-16 10:19:49 +0100

    Zarejestruj toolbar i paginację w magento-catalog interceptorach
    CategoryProductList już renderuje te komponenty wewnętrznie.
    Musimy je zarejestrować aby były dostępne w componentRegistry dla SSR.

--- app/modules/renia/magento-catalog/interceptors/default.ts
+++ app/modules/renia/magento-catalog/interceptors/default.ts
@@ -1,9 +1,13 @@
 // @env: mixed
 import { CategoryProductList } from '../components/CategoryProductList';
+import { ProductListingToolbar } from '../components/ProductListingToolbar';
+import { ProductListingPagination } from '../components/ProductListingPagination';
 import '../services/storefrontConfigAugmenter';
 
 export default (api: any) => {
   api.registerComponents?.({
-    'renia-magento-catalog/components/CategoryProductList': CategoryProductList
+    'renia-magento-catalog/components/CategoryProductList': CategoryProductList,
+    'renia-magento-catalog/components/ProductListingToolbar': ProductListingToolbar,
+    'renia-magento-catalog/components/ProductListingPagination': ProductListingPagination
   });
 };
